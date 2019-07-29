d23a9299a0a3cc7a546018c329410af7b77ffa95
Eric <ericgeppert@att.net>
2019-07-29 15:21:39 -0500

    added document preview if isAthenticated=false

--- src/components/MyPDFDocument.js
+++ src/components/MyPDFDocument.js
@@ -1,78 +1,58 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { Document, Page } from 'react-pdf';
 
 class MyPDFDocument extends Component {
-    rednerWorkout(param) {
-        switch(param){
-            case("../images/workouts/ConditioningWeightLoss5V1.pdf"):
-                return(
-                    require("../images/workouts/ConditioningWeightLoss5V1.pdf")
-                );
-            case("../images/workouts/ConditioningWeightLoss4V1.pdf"):
-                return(
-                    require("../images/workouts/ConditioningWeightLoss4V1.pdf")
-                );
-            case("../images/workouts/ConditioningWeightLoss3V1.pdf"):
-                return(
-                    require("../images/workouts/ConditioningWeightLoss3V1.pdf")
-                );
-            case("../images/workouts/AtHomeTotalBody5V1.pdf"):
-                return(
-                    require("../images/workouts/AtHomeTotalBody5V1.pdf")
-                );
-            case("../images/workouts/AtHomeTotalBody4V1.pdf"):
-                return(
-                    require("../images/workouts/AtHomeTotalBody4V1.pdf")
-                );
-            case("../images/workouts/AtHomeTotalBody3V1.pdf"):
-                return(
-                    require("../images/workouts/AtHomeTotalBody3V1.pdf")
-                );
-            case("../images/workouts/FunctionalFitnessHIIT5V1.pdf"):
-                return(
-                    require("../images/workouts/FunctionalFitnessHIIT5V1.pdf")
-                );
-            case("../images/workouts/FunctionalFitnessHIIT4V1.pdf"):
-                return(
-                    require("../images/workouts/FunctionalFitnessHIIT4V1.pdf")
-                );
-            case("../images/workouts/FunctionalFitnessHIIT3V1.pdf"):
-                return(
-                    require("../images/workouts/FunctionalFitnessHIIT3V1.pdf")
-                );
-            case("../images/workouts/TotalBodyTransformation5V1.pdf"):
-                return(
-                    require("../images/workouts/TotalBodyTransformation5V1.pdf")
-                );
-            case("../images/workouts/TotalBodyTransformation4V1.pdf"):
-                return(
-                    require("../images/workouts/TotalBodyTransformation4V1.pdf")
-                );
-            case("../images/workouts/TotalBodyTransformation3V1.pdf"):
-                return(
-                    require("../images/workouts/TotalBodyTransformation3V1.pdf")
-                );
-            default:
-                console.log("workout Not found in PDFdocument switch statement");
-        }
+  rednerWorkout(param) {
+    switch (param) {
+      case '../images/workouts/ConditioningWeightLoss5V1.pdf':
+        return require('../images/workouts/ConditioningWeightLoss5V1.pdf');
+      case '../images/workouts/ConditioningWeightLoss4V1.pdf':
+        return require('../images/workouts/ConditioningWeightLoss4V1.pdf');
+      case '../images/workouts/ConditioningWeightLoss3V1.pdf':
+        return require('../images/workouts/ConditioningWeightLoss3V1.pdf');
+      case '../images/workouts/AtHomeTotalBody5V1.pdf':
+        return require('../images/workouts/AtHomeTotalBody5V1.pdf');
+      case '../images/workouts/AtHomeTotalBody4V1.pdf':
+        return require('../images/workouts/AtHomeTotalBody4V1.pdf');
+      case '../images/workouts/AtHomeTotalBody3V1.pdf':
+        return require('../images/workouts/AtHomeTotalBody3V1.pdf');
+      case '../images/workouts/FunctionalFitnessHIIT5V1.pdf':
+        return require('../images/workouts/FunctionalFitnessHIIT5V1.pdf');
+      case '../images/workouts/FunctionalFitnessHIIT4V1.pdf':
+        return require('../images/workouts/FunctionalFitnessHIIT4V1.pdf');
+      case '../images/workouts/FunctionalFitnessHIIT3V1.pdf':
+        return require('../images/workouts/FunctionalFitnessHIIT3V1.pdf');
+      case '../images/workouts/TotalBodyTransformation5V1.pdf':
+        return require('../images/workouts/TotalBodyTransformation5V1.pdf');
+      case '../images/workouts/TotalBodyTransformation4V1.pdf':
+        return require('../images/workouts/TotalBodyTransformation4V1.pdf');
+      case '../images/workouts/TotalBodyTransformation3V1.pdf':
+        return require('../images/workouts/TotalBodyTransformation3V1.pdf');
+      default:
+        console.log('workout Not found in PDFdocument switch statement');
     }
+  }
 
-    renderPage(){
-        var pageElements = [];
-        for(var i =1;i< this.props.pages;i++)
-            pageElements.push(<Page key={i} pageNumber={i} onLoadError={console.error}/>)
-        return(pageElements);
-        
-    }
+  renderPage() {
+    var pageElements = [];
+    const isAuthenticated = false;
+    if (isAuthenticated) var pageCount = this.props.pages;
+    else var pageCount = 2;
 
-    render(){
-        var totalPages=5;
-        return(
-            <Document file={this.rednerWorkout(this.props.file)}>
-                {this.renderPage()}
-          </Document>
-        );
-    }
+    for (var i = 1; i < pageCount; i++)
+      pageElements.push(
+        <Page key={i} pageNumber={i} onLoadError={console.error} />
+      );
+    return pageElements;
+  }
+
+  render() {
+    return (
+      <Document file={this.rednerWorkout(this.props.file)}>
+        {this.renderPage()}
+      </Document>
+    );
+  }
 }
 
 export default MyPDFDocument;
